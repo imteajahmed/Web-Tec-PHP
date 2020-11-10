4da636c1f457cf40ddf803e7d9586f8ac1863720
imteajahmed <imteajshoscho52@gmail.com>
2020-11-10 17:21:57 +0600

    loin form

---
+++ Login.php
@@ -0,0 +1,25 @@
+<html>
+     <head>
+	       <title>Login Form</title>
+	 </head>
+	 <body>
+	      <h1>Login Form</h1>
+		  <form>
+		       <table>
+			        <tr>
+					  <td>Username:</td>
+					  <td><input type="text"></td>
+					</tr>
+					<tr>
+					  <td>Password:</td>
+					  <td><input type="password"></td>
+					</tr>
+					<tr>
+					  <td Colspan="2" align="right">
+					      <input type="submit" name="Login" value="Login">;
+					  </td>
+					</tr>
+			   </table>
+		  </form>
+	 </body>
+</html>
